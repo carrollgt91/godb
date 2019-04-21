55a4530469bb5d205a3f285aa49caf27ce7d10d0
Grayson Carroll <grayson.carroll@satchelhealth.com>
2019-04-21 12:01:50 -0500

    Adds basic project structuring

---
+++ inputbuffer.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+)
+
+/*
+	InputBuffer is a wrapper around taking input from the user via
+	the command prompt.
+*/
+type InputBuffer struct {
+	Buffer string
+	reader *bufio.Reader
+}
+
+/*
+	NewInputBuffer creates a new InputBuffer that reads from Stdin
+*/
+func NewInputBuffer() *InputBuffer {
+	ib := &InputBuffer{
+		reader: bufio.NewReader(os.Stdin),
+	}
+
+	return ib
+}
+
+/*
+	ReadLine reads a single line of user input and returns it, or an error if one exists.
+*/
+func (ib *InputBuffer) ReadLine() (string, error) {
+	text, err := ib.reader.ReadString('\n')
+
+	if err != nil {
+		return "", err
+	}
+
+	// Remove trailing newline
+	text = strings.Replace(text, "\n", "", -1)
+	return text, nil
+}
--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
+// Entrypoint
 func main() {
+	ib := NewInputBuffer()
 	fmt.Println("Running godb...")
+
+	for {
+		// Read from buffer until we hit newline character
+		text, err := ib.ReadLine()
+		if err != nil {
+			fmt.Printf("Error reading line: %v\n", err)
+			os.Exit(ReadLineErrorCode)
+		}
+
+		if strings.Compare(".exit", text) == 0 {
+			fmt.Println("Goodbye.")
+			// exit with status code 0
+			os.Exit(0)
+			return
+		}
+
+		fmt.Printf("Error: Command Not recognized %s\n", text)
+	}
 }
